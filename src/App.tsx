77b78c214b50e5a3c741c72826bffe2d151d7630
dxenia <marascadanaxenia@gmail.com>
2023-09-13 08:29:40 +0200

    created preliminary routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,19 @@
+import { Route, Routes } from 'react-router-dom';
 
-import './App.css'
+import Home from '../src/pages/Home/Home';
+import About from './pages/About/About';
+
+import './App.css';
 
 function App() {
-
-
   return (
     <>
-    
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
