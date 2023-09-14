7dda0ed8b746c2482a5884f1a3e6ae3c52a698e2
dxenia <marascadanaxenia@gmail.com>
2023-09-14 10:02:46 +0200

    added heading component

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,6 +1,10 @@
 import zodiacWheel from '../../assets/images/zodiac-wheel.png';
 import moonPhases from '../../assets/images/moon-phases.png';
+import butterfly from '../../assets/images/butterfly.png';
+import butterflyTwo from '../../assets/images/butterfly-2.png';
+import moth from '../../assets/images/moth.png';
 
+import Heading from '../../components/Heading/Heading';
 import Button from '../../components/Button/Button';
 
 import './Home.css';
@@ -10,8 +14,10 @@ export default function Home() {
     <div className="home__scroll">
       <section className="home__intro">
         <div className="home__intro--info">
-          <h1 className="home__heading">lunar☾</h1>
-          <h3 className="home__intro--heading">Astrology in your pocket.</h3>
+          <Heading element="h1">lunar☾</Heading>
+          <Heading element="h3" className="home__intro--heading">
+            Astrology in your pocket.
+          </Heading>
           <p className="home__intro--subheading">
             Welcome to the mystical world of Lunar, your gateway to unlocking
             the secrets of the cosmos and discovering the hidden truths that lie
@@ -27,12 +33,10 @@ export default function Home() {
         <img src={zodiacWheel} className="home__intro--img"></img>
       </section>
       <section className="home__main">
-        <img src={moonPhases} className="home__main--img"></img>
-        <h3 className="home__main--heading">
-          What can you find on our website?
-        </h3>
-        <div className="home__main--info">
-          <article className="home__main--item">
+        <Heading element="h4">What can you find on Lunar?</Heading>
+        <ul className="home__main--info">
+          <li className="home__main--item">
+            <img src={moth} className="home__main--icon"></img>
             Learn About Zodiac Signs: "Embark on a celestial journey to explore
             the profound world of Zodiac signs. Dive deep into the unique
             characteristics, traits, and mysteries of each astrological sign.
@@ -41,8 +45,9 @@ export default function Home() {
             resources and insightful articles will guide you through the
             captivating realm of astrology, helping you understand yourself and
             the people around you like never before."
-          </article>
-          <article className="home__main--item">
+          </li>
+          <li className="home__main--item">
+            <img src={butterfly} className="home__main--icon"></img>
             Learn About Tarot Cards: "Unlock the ancient wisdom of Tarot cards
             and delve into the intriguing art of divination. Explore the
             symbolism, meanings, and interpretations of each Tarot card in our
@@ -51,8 +56,9 @@ export default function Home() {
             your understanding of this mystical practice. Gain valuable insights
             into your past, present, and future as you navigate the enchanting
             world of Tarot cards."
-          </article>
-          <article className="home__main--item">
+          </li>
+          <li className="home__main--item">
+            <img src={butterflyTwo} className="home__main--icon"></img>
             Get a Card Reading: "Experience the magic of a personalized Tarot
             card reading right at your fingertips. Connect with our skilled and
             intuitive readers who will provide you with a one-of-a-kind Tarot
@@ -61,8 +67,9 @@ export default function Home() {
             inspiration. Our readings are designed to empower you, offering
             profound insights and a deeper connection to your spiritual journey.
             Let the Tarot unveil the secrets of your destiny today."
-          </article>
-        </div>
+          </li>
+        </ul>
+        <img src={moonPhases} className="home__main--img"></img>
       </section>
     </div>
   );
