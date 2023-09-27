7306456f862725585886c3d3aec086aa2b953e6c
dxenia <marascadanaxenia@gmail.com>
2023-09-27 11:36:42 +0200

    fixed pagination issue

--- src/components/Pagination/Pagination.tsx
+++ src/components/Pagination/Pagination.tsx
@@ -9,57 +9,15 @@ export const Pagination: React.FC<PaginationProps> = ({
   return (
     <div>
       <div className="pagination">
-        {page !== 1 && (
+        {page > 1 && (
           <button onClick={() => handlePagination(page - 1)} type="button">
-            &lt;
+            Back
           </button>
         )}
 
-        <button onClick={() => handlePagination(1)} type="button">
-          {1}
-        </button>
-
-        {page > 3 && <div>...</div>}
-
-        {page === totalPages && totalPages > 3 && (
-          <button onClick={() => handlePagination(page - 2)} type="button">
-            {page - 2}
-          </button>
-        )}
-
-        {page > 2 && (
-          <button onClick={() => handlePagination(page - 1)} type="button">
-            {page - 1}
-          </button>
-        )}
-
-        {page !== 1 && page !== totalPages && (
-          <button onClick={() => handlePagination(page)} type="button">
-            {page}
-          </button>
-        )}
-
-        {page < totalPages - 1 && (
+        {page < totalPages && (
           <button onClick={() => handlePagination(page + 1)} type="button">
-            {page + 1}
-          </button>
-        )}
-
-        {page === 1 && totalPages > 3 && (
-          <button onClick={() => handlePagination(page + 2)} type="button">
-            {page + 2}
-          </button>
-        )}
-
-        {page < totalPages - 2 && <div>...</div>}
-
-        <button onClick={() => handlePagination(totalPages)} type="button">
-          {totalPages}
-        </button>
-
-        {page !== totalPages && (
-          <button onClick={() => handlePagination(page + 1)} type="button">
-            &gt;
+            Forward
           </button>
         )}
       </div>
--- src/pages/Tarot/Tarot.tsx
+++ src/pages/Tarot/Tarot.tsx
@@ -9,43 +9,43 @@ import '../Tarot/Tarot.css';
 
 export default function Tarot() {
   const [page, setPage] = useState(1);
-  const [filterCriteria, setFilterCriteria] = useState('All'); // Initialize with 'All'
+  const [filterCriteria, setFilterCriteria] = useState('All');
   const itemsPerPage = 10;
 
   const url = 'https://jps-tarot-api.azurewebsites.net/api/Tarot/Get';
   const { data: cards, loading, error } = useFetch<TarotProps[]>(url);
 
-  const totalPages = Math.ceil((cards?.length || 0) / itemsPerPage);
+  const filteredCards = cards?.filter((card) => {
+    if (filterCriteria === 'All') {
+      return true;
+    }
+    return card.type === filterCriteria;
+  });
+
+  const totalFilteredPages = Math.ceil(
+    (filteredCards?.length || 0) / itemsPerPage
+  );
 
   const startIndex = (page - 1) * itemsPerPage;
   const endIndex = startIndex + itemsPerPage;
-
-  // Filtering function based on the selected criteria
-  const filteredCards = cards?.filter((card) => {
-    if (filterCriteria === 'All') {
-      return true; // Show all cards
-    }
-    return card.type === filterCriteria; // Filter by type
-  });
 
   const cardsToDisplay = filteredCards?.slice(startIndex, endIndex) || [];
 
   useEffect(() => {
     if (page < 1) {
       setPage(1);
-    } else if (page > totalPages) {
-      setPage(totalPages);
+    } else if (page > totalFilteredPages) {
+      setPage(totalFilteredPages);
     }
-  }, [page, totalPages, loading, error]);
+  }, [page, totalFilteredPages, loading, error]);
 
   if (error) {
     console.log(`Error: ${error.message}`);
   }
 
-  // Handler for changing the filter criteria
   const handleFilterChange = (criteria: string) => {
     setFilterCriteria(criteria);
-    setPage(1); // Reset to the first page when changing the filter
+    setPage(1);
   };
 
   return (
@@ -53,8 +53,6 @@ export default function Tarot() {
       <h2>Tarot Cards</h2>
       {error && <div>{error?.message}</div>}
       {loading && <Loading />}
-
-      {/* Filter Buttons */}
       <div className="filter-buttons">
         <button onClick={() => handleFilterChange('All')}>All</button>
         <button onClick={() => handleFilterChange('Major')}>
@@ -63,7 +61,6 @@ export default function Tarot() {
         <button onClick={() => handleFilterChange('Minor')}>
           Minor Arcana
         </button>
-        {/* Add more buttons for other filter criteria */}
       </div>
 
       <div className="card__list">
@@ -73,7 +70,7 @@ export default function Tarot() {
       </div>
       <Pagination
         page={page}
-        totalPages={totalPages}
+        totalPages={totalFilteredPages} // Use the total pages of filtered cards
         handlePagination={setPage}
       />
     </div>
