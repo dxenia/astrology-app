4af49d3e8485ff6d55500642ae6657829ec85848
dxenia <marascadanaxenia@gmail.com>
2023-09-28 23:49:43 +0200

    edited the image carousel

--- src/components/ImageCarousel/ImageCarousel.tsx
+++ src/components/ImageCarousel/ImageCarousel.tsx
@@ -19,25 +19,34 @@ export default function Carousel() {
   }
 
   return (
-    <>
+    <div className="carousel-container">
       <nav className="box">
-        <button onClick={() => scrollToIndex(0)}>Tom</button>
-        <button onClick={() => scrollToIndex(1)}>Maru</button>
-        <button onClick={() => scrollToIndex(2)}>Jellylorum</button>
+        <button onClick={() => scrollToIndex(0)}>Astrology</button>
+        <button onClick={() => scrollToIndex(1)}>Tarots</button>
+        <button onClick={() => scrollToIndex(2)}>Numerology</button>
       </nav>
       <div>
         <ul ref={listRef} className="box-ul">
           <li>
-            <img src="https://placekitten.com/g/200/200" alt="Tom" />
+            <img
+              src="https://media3.giphy.com/media/v1.Y2lkPTc5MGI3NjExaHdsc3pkNXcyZ21tdmoxZnc5cmJkNDl5emxnOG5taW83dGRzZzNiMSZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9cw/3o85xKklNs4DxiFAsM/giphy.gif"
+              alt="Abstract face"
+            />
           </li>
           <li>
-            <img src="https://placekitten.com/g/300/200" alt="Maru" />
+            <img
+              src="https://media3.giphy.com/media/v1.Y2lkPTc5MGI3NjExaGV1Ymg4aG02cGlwc3l0aWlwMWdxb2w1bWd4OWFoazVld3A3YWs4ZyZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9cw/vwGbmfK9TYfDd3FSca/giphy.gif"
+              alt="Abstract face"
+            />
           </li>
           <li>
-            <img src="https://placekitten.com/g/250/200" alt="Jellylorum" />
+            <img
+              src="https://media1.giphy.com/media/v1.Y2lkPTc5MGI3NjExeXhiMnhhbGxnd2p0dHJ1Y2FwYjhnaHI5bXAxZWFsbXB5eW1panAzMCZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9cw/pL6w6uW41bzQA/giphy.gif"
+              alt="Abstract face"
+            />
           </li>
         </ul>
       </div>
-    </>
+    </div>
   );
 }
