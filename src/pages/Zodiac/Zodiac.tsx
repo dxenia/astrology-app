96d99ec4265550fd301090d31bd5e914868925f2
dxenia <marascadanaxenia@gmail.com>
2023-10-05 17:09:45 +0200

    image update for zodiac page and affiliated details

--- src/pages/Zodiac/Zodiac.tsx
+++ src/pages/Zodiac/Zodiac.tsx
@@ -1,3 +1,4 @@
+import owlHead from '../../assets/images/owl-head.webp';
 import useFetch from '../../hooks/useFetch';
 import ZodiacCard from '../../components/ZodiacCard/ZodiacCard.tsx';
 import Loading from '../../components/Loading/Loading.tsx';
@@ -16,27 +17,26 @@ function Zodiac() {
   }
 
   return (
-    <>
-      <div>
-        <h1>Astrology</h1>
-        <p className="description">
-          Astrology is an ancient and complex system of divination that has been
-          practiced for thousands of years. Its origins can be traced back to
-          various civilizations throughout history, including Mesopotamia,
-          Egypt, and Greece. The Western zodiac, also known as the tropical
-          zodiac, is based on the position of the Sun relative to the twelve
-          zodiac signs at the time of a person's birth. Each sign is associated
-          with specific personality traits and characteristics.
-        </p>
-        {error && <div>{error?.message}</div>}
-        {loading && <Loading />}
-        <div className="astrology__list">
-          {signs?.map((sign) => (
-            <ZodiacCard key={sign.id} sign={sign} />
-          ))}
-        </div>
+    <section>
+      <h1>Astrology</h1>
+      {error && <div>{error?.message}</div>}
+      {loading && <Loading />}
+      <p className="description">
+        Astrology is an ancient and complex system of divination that has been
+        practiced for thousands of years. Its origins can be traced back to
+        various civilizations throughout history, including Mesopotamia, Egypt,
+        and Greece. The Western zodiac, also known as the tropical zodiac, is
+        based on the position of the Sun relative to the twelve zodiac signs at
+        the time of a person's birth. Each sign is associated with specific
+        personality traits and characteristics.
+      </p>
+      <img src={owlHead} className="astrology__image" alt="Owl headed man" />
+      <div className="astrology__list">
+        {signs?.map((sign) => (
+          <ZodiacCard key={sign.id} sign={sign} />
+        ))}
       </div>
-    </>
+    </section>
   );
 }
 
--- src/pages/ZodiacSignDetails/ZodiacSignDetails.tsx
+++ src/pages/ZodiacSignDetails/ZodiacSignDetails.tsx
@@ -1,5 +1,6 @@
 import { useParams, useNavigate } from 'react-router-dom';
 
+import snake from '../../assets/images/snake.png';
 import useFetch from '../../hooks/useFetch';
 import Button from '../../components/Button/Button.tsx';
 import Loading from '../../components/Loading/Loading.tsx';
@@ -34,14 +35,15 @@ function ZodiacSignDetails() {
         </div>
         <ul>
           <li>
-            <h3>Celestial body:</h3> {sign?.ruler.type}, {sign?.ruler.name}
+            <h3>Ruler:</h3>
+            {sign?.ruler.name}
             <img src={sign?.ruler.image}></img>
           </li>
           <li>
             <h3>Transition consistency:</h3> {sign?.ruler.transition}.
           </li>
           <li>
-            <h3>Relevant keywords of the luminary:</h3>{' '}
+            <h3>Relevant keywords of the planet:</h3>{' '}
             {sign?.ruler.keywords?.join(', ')}.
           </li>
 
@@ -52,6 +54,7 @@ function ZodiacSignDetails() {
             <h3>Sign's Negative Traits</h3> {sign?.negativeTraits?.join(', ')}
           </li>
         </ul>
+        <img src={snake} className="snake-img" alt="Snake" />
         <Button
           className="card__details__back-btn"
           onClick={() => navigate(-1)}
