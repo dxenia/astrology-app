9aa18c520cb636f8df275a2846efa5be0d54a06b
dxenia <marascadanaxenia@gmail.com>
2023-10-15 07:44:31 +0200

    accordion data edit

--- src/data/accordionData.ts
+++ src/data/accordionData.ts
@@ -15,15 +15,14 @@ export const accordionData = [
   },
   {
     title: 'Requirements',
-    content: `The application needs to meet several requirements! The topic is chosen by each student
-    individually. Each week, students need to prove to have understood, learned and implemented new topics.
+    content: `The application needs to meet several requirements. Each week, new topics must be implemented.
     Applications must be deployed at the end. No external package is allowed other than React and React Router
     Dom. The application must fetch data through an API. The application needs to be fully
     functional on the deployed version.`,
   },
   {
     title: 'Checkpoints',
-    content: `The weekly checkpoints help the students build the application on a consistent basis. The following
+    content: `The weekly checkpoints help to build the application on a consistent basis. The following
     checkpoints need to be met: inclusion of type-safe components, hooks(useState, useEffect, useRef, useCallback, useMemo),
     DOM events, conditional rendering and list rendering, react-router-dom(and navigation between at least two pages), deployment.`,
   },
